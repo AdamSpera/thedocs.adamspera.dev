d6bcaecb8ddbe1284246a190893b8eb4f2b51a61
adamspera <adamspera@hotmail.com>
2023-12-20 18:01:52 -0500

    Refactor search functionality and update styles

--- search.js
+++ search.js
@@ -28,13 +28,21 @@ $(document).ready(function () {
     for (let i = 0; i < searchResults.length; i++) {
       var filePath = searchResults[i].substring(1);
 
-      // Add spaces around the slashes in the file path
-      var formattedFilePath = filePath.replace(/\//g, ' / ').replace('.md', '');
+      // Split the file path into the directory and the file name
+      var lastSlashIndex = filePath.lastIndexOf('/');
+      var directory = filePath.substring(0, lastSlashIndex);
+      var fileName = filePath.substring(lastSlashIndex + 1);
 
-      // Make the query bold
-      var boldedFilePath = formattedFilePath.replace(new RegExp(query, 'gi'), '<strong>$&</strong>');
+      // Add spaces around the slashes in the directory
+      var formattedDirectory = directory.replace(/\//g, ' / ');
 
-      let result = $('<p class="result-blob"> <img src="icons/file-open.svg" alt="File" class="icon-result">' + boldedFilePath + '</p>');
+      // Make the query bold in the file name
+      var boldedFileName = fileName.replace(new RegExp(query, 'gi'), '<strong>$&</strong>');
+
+      // Combine the directory and the bolded file name
+      var formattedFilePath = formattedDirectory + ' / ' + boldedFileName.replace('.md', '');
+
+      let result = $('<p class="result-blob"> <img src="icons/file-open.svg" alt="File" class="icon-result">' + formattedFilePath + '</p>');
       result.click(async function () {
         console.log(filePath);
         await fetchAndDisplayMarkdown(filePath);
@@ -51,7 +59,7 @@ function searchFiles(directory, query, path = '') {
   let results = [];
   for (let [name, type] of Object.entries(directory)) {
     let fullPath = path + '/' + name;
-    if (type === 'file' && fullPath.toLocaleLowerCase().includes(query.toLocaleLowerCase())) {
+    if (type === 'file' && name.toLocaleLowerCase().includes(query.toLocaleLowerCase())) {
       results.push(fullPath);
     } else if (type !== 'file') {
       results = results.concat(searchFiles(type, query, fullPath));
