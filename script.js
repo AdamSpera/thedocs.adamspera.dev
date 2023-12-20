1c0339cba2841679201e743038d266c9ca2ab622
adamspera <adamspera@hotmail.com>
2023-12-20 14:40:34 -0500

    Add new icons and update sidebar layout

--- script.js
+++ script.js
@@ -85,29 +85,52 @@ function createTree(structure, parentElement, parentPath = '') {
 }
 
 function createListItem(key, value, currentPath) {
-    const li = $('<li></li>').text(key.replace('.md', ''));
+    const li = $('<li></li>');
+    key = key.replace('.md', '');
     if ($.isPlainObject(value)) {
-        handleDirectory(li, value, currentPath);
+        handleDirectory(li, value, currentPath, key);
     } else {
-        handleFile(li, currentPath);
+        handleFile(li, currentPath, key);
     }
     return li;
 }
 
-function handleDirectory(li, value, currentPath) {
+function handleDirectory(li, value, currentPath, key) {
+    $(li).html('<img src="icons/folder-solid.svg" alt="Folder" class="icon">' + key);
+
     const subUl = $('<ul></ul>').css('display', 'none');
     createTree(value, subUl, currentPath);
     li.append(subUl);
     li.css('cursor', 'pointer');
+
     li.click((event) => {
         event.stopPropagation();
         subUl.slideToggle();
+        // Find the image and toggle its source
+        var img = $(li).find('img').first();
+        var currentIcon = img.attr('src');
+        var newIcon = currentIcon === 'icons/folder-solid.svg' ? 'icons/folder-open.svg' : 'icons/folder-solid.svg';
+        img.attr('src', newIcon);
     });
 }
 
-function handleFile(li, currentPath) {
+function handleFile(li, currentPath, key) {
+    $(li).html('<img src="icons/file-solid.svg" alt="File" class="icon">' + key);
+
     li.click((event) => {
         event.stopPropagation();
+
+        // Change the source of all images with a source of 'icons/file-solid.svg' to 'icons/file-regular.svg'
+        $('img[src="icons/file-regular.svg"]').each(function () {
+            $(this).attr('src', 'icons/file-solid.svg');
+        });
+
+        // Find the image and toggle its source
+        var img = $(li).find('img').first();
+        var currentIcon = img.attr('src');
+        var newIcon = currentIcon === 'icons/file-solid.svg' ? 'icons/file-regular.svg' : 'icons/file-solid.svg';
+        img.attr('src', newIcon);
+
         $('.sidebar-widget, .tree li').removeClass('active');
         li.addClass('active');
         fetchAndDisplayMarkdown(currentPath);
