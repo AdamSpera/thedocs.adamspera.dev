a747541bdbc549e1956e54dc5139ab6945b3be6b
adamspera <adamspera@hotmail.com>
2023-12-23 23:10:24 -0500

    Add MIT License and new icon file

--- search.js
+++ search.js
@@ -27,13 +27,13 @@ $(document).ready(function () {
 });
 
 function fileNameSearch(query) {
-
   // Search the directory structure for files that contain the query
   var searchResults = searchFiles(directoryStructure, query);
 
   // Display the search results
-  for (let i = 0; i < searchResults.length; i++) {
-    var filePath = searchResults[i].substring(1);
+  searchResults.forEach((result) => {
+    // Remove the first character from the result
+    var filePath = result.substring(1);
 
     // Split the file path into the directory and the file name
     var lastSlashIndex = filePath.lastIndexOf('/');
@@ -46,21 +46,30 @@ function fileNameSearch(query) {
     // Make the query bold in the file name
     var boldedFileName = fileName.replace(new RegExp(query, 'gi'), '<strong>$&</strong>').replace('.md', '');
 
-    let result = $('<p class="result-blob"> <span style="font-size: 13px">' + formattedDirectory + '</span> <br> <img src="icons/file-solid.svg" alt="File" class="icon-result">' + boldedFileName + '</p>');
-    result.click(async function () {
+    // Create a new paragraph element with the class 'result-blob'
+    let resultElement = $(`
+      <p class="result-blob">
+        <span style="font-size: 13px">${formattedDirectory}</span><br>
+        <img src="icons/file-solid.svg" alt="File" class="icon-result">${boldedFileName}
+      </p>
+    `);
+
+    // Add a click event listener to the result element
+    resultElement.click(async function () {
       console.log(filePath);
       $("img[src='icons/file-open.svg']").attr("src", "icons/file-solid.svg");
       await fetchAndDisplayMarkdown(filePath);
       $('.sidebar-widget, .tree li').removeClass('active');
       $('#searchModal').modal('hide');
     });
-    $("#searchResults").append(result);
-  }
 
+    // Append the result element to the search results
+    $("#searchResults").append(resultElement);
+  });
 }
 
-async function fileKeySearch(input) {
-  input = input.replace(/ /g, '%20');
+async function fileKeySearch(query) {
+  query = query.replace(/ /g, '%20');
   let mdFiles = searchFiles(directoryStructure, '.md');
   let matchingFiles = new Map();
 
@@ -72,11 +81,11 @@ async function fileKeySearch(input) {
 
     $(html).find('img').each(function () {
       let src = $(this).attr('src');
-      input = input.toLowerCase();
+      query = query.toLowerCase();
       if (src.includes("https://img.shields.io/badge/") && src.includes("-darkgreen")) {
         var badgeText = src.split("https://img.shields.io/badge/")[1].split("-darkgreen")[0];
         var lowerBadgeText = badgeText.toLowerCase();
-        if (lowerBadgeText.includes(input)) {
+        if (lowerBadgeText.includes(query)) {
           if (!matchingFiles.has(url)) {
             matchingFiles.set(url, []);
           }
@@ -86,17 +95,28 @@ async function fileKeySearch(input) {
     });
   }
   for (let [url, badges] of matchingFiles.entries()) {
+    // Replace underscores with spaces and remove '.md' from the URL
     let formattedFilePath = url.replace(/_/g, ' ').replace('.md', '');
-    formattedFilePath = formattedFilePath.replace(/\//g, ' / '); // Add spaces around '/'
-    let result = $('<p class="result-blob" style="display: flex; flex-wrap: wrap;"></p>');
-    
+
+    // Add spaces around '/'
+    formattedFilePath = formattedFilePath.replace(/\//g, ' / ');
+
+    // Find the last index of ' / ' in the formatted file path
     let lastSlashIndex = formattedFilePath.lastIndexOf(' / ');
+
+    // Get the file path and the text after the last slash
     let filePath = formattedFilePath.substring(0, lastSlashIndex);
     let textAfterLastSlash = formattedFilePath.substring(lastSlashIndex + 3);
 
-    let textElement = $('<span style="padding-right: 11px"> <span style="font-size: 13px">' + filePath + '</span> <br> <span>' + textAfterLastSlash + '</span></span>');
-    result.append(textElement);
-    let badgesElement = $('<span></span>');
+    // Create a new paragraph element with the class 'result-blob'
+    let result = $(
+      `<p class="result-blob">
+        <span style="font-size: 13px">${filePath}</span><br>
+        <img src="icons/lightbulb-solid.svg" alt="File" class="icon-result">${textAfterLastSlash}
+      </p>`
+    );
+
+    let badgesElement = $('<span><br></span>');
     for (let badgeText of badges) {
       badgesElement.append('<img src="https://img.shields.io/badge/' + badgeText + '-darkgreen" alt="Badge" class="icon-tag">');
     }
