8f0273ebfd97042b2aeca9e7a7f4cdff6f34fc46
adamspera <adamspera@hotmail.com>
2023-12-26 16:04:27 -0500

    Refactor event handlers and cache clearing functionality

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,28 +1,35 @@
+// Constants for the user, repo, and start folder
 const user = "adamspera";
 const repo = "thedocs.adamspera.dev";
 const start_folder = "content";
 const base_url = `https://api.github.com/repos/${user}/${repo}/contents/${start_folder}`;
 
+// Initialize the app when the document is ready
 $(document).ready(initializeApp);
 
+// This function sets up the event handlers and fetches the directory contents
 function initializeApp() {
     setupEventHandlers();
     fetchAndDisplayDirectoryContents();
 }
 
+// This function sets up the event handlers for the GitHub link, explore button, and clear cache button
 function setupEventHandlers() {
     $('#github-link').click(() => window.location.href = "https://github.com/AdamSpera/thedocs.adamspera.dev");
     $('#explore').click(() => {
         $('#section-documentviewer').empty();
         $('#section-fileexplorer').show();
     });
-    $('#clear-cache').click(async () => {
-        const cache = await caches.open('docs-cache');
-        await cache.delete('structure');
-        alert('Cache cleared!');
-    });
 }
 
+// This function clears the 'structure' cache
+async function clearCache() {
+    const cache = await caches.open('docs-cache');
+    await cache.delete('structure');
+    alert('Cache cleared!');
+}
+
+// This function fetches and displays the directory contents
 async function fetchAndDisplayDirectoryContents() {
     try {
         const cache = await caches.open('docs-cache');
@@ -45,6 +52,7 @@ async function fetchAndDisplayDirectoryContents() {
     }
 }
 
+// This function fetches the directory contents from the GitHub API
 async function fetchDirectoryContents(url) {
     const response = await $.ajax({ url: url, method: 'GET' });
     let directoryStructure = {};
@@ -60,21 +68,25 @@ async function fetchDirectoryContents(url) {
     return directoryStructure;
 }
 
+// This function retrieves the cached structure
 async function getCachedStructure(cache, key) {
     const cachedResponse = await cache.match(key);
     return cachedResponse ? await cachedResponse.json() : null;
 }
 
+// This function checks if a timestamp is over a day old
 function isOverADayOld(timestamp) {
     const oneDay = 24 * 60 * 60 * 1000; // milliseconds in a day
     const now = Date.now();
     return now - timestamp > oneDay;
 }
 
+// This function caches the structure
 async function cacheStructure(cache, key, data) {
     await cache.put(key, new Response(JSON.stringify(data)));
 }
 
+// This function fetches and displays the markdown
 async function fetchAndDisplayMarkdown(url) {
     $.get(`content/${url}`, (data) => {
         const converter = new showdown.Converter();
